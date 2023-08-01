0f8021a9c4bc0f54c25670ca7c021af67e4c054b
praveensharma-motherson <139841882+praveensharma-motherson@users.noreply.github.com>
2023-08-01 12:56:09 +0530

    Add files via upload
    Files added to repo

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(477, 136);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(204, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Add to Cache";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(477, 186);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(180, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Read Cache";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using Redis.OM;
+using Redis.OM.Modeling;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var provider = new RedisConnectionProvider("redis://127.0.0.1:6379");
+
+            var connection = provider.Connection;
+            var sessionKeys = provider.RedisCollection<SessionKey>();
+            var sessKey1 = new SessionKey { Id = "User1:Instance1", Name = "AllSessionKeys", Value = "JSON VALUES1 would be here " };
+            var sessKey2 = new SessionKey { Id = "User1:Instance2", Name = "AllSessionKeys", Value = "JSON VALUES2 would be here" };
+            var idp1 = connection.SetAsync(sessKey1);
+            var idp2 = sessionKeys.InsertAsync(sessKey2);
+
+            var reconstitutedE1 = connection.GetAsync<SessionKey>(sessKey1.Id);
+            //connection.JsonSet("json1","this is path","{Item{1,2,3},Item{4,5,6}}");
+
+
+            var xx = connection.HGetAll("SessionKey:User1:Instance1");
+
+            //update a cache key in redis
+            //connection.JsonSet("json1", "Item[0]", "100");
+
+            //var xx2 = connection.JsonGet("json1", "Item[0]");   
+
+            MessageBox.Show(reconstitutedE1.ToString());
+        }
+
+        [Document(Prefixes = new[] { "SessionKey" })]
+        public class SessionKey
+        {
+            [RedisIdField]
+            public string Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string Value { get; set; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var provider = new RedisConnectionProvider("redis://127.0.0.1:6379");
+
+            var connection = provider.Connection;
+            
+
+            var xx = connection.HGetAll("SessionKey:User1:Instance1");
+
+            MessageBox.Show(xx.ToString());
+
+        }
+    }
+}
